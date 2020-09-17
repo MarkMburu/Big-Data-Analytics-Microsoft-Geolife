049ec42443b364ce2732c449b89772c8031fcaf5
MarkMburu <markmburu685@gmail.com>
2020-09-17 08:32:26 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='microsoft geo project',
+    version='0.1',
+    long_description='microsoft geo project',
+    packages=find_packages(),
+    install_requires=[],
+)
